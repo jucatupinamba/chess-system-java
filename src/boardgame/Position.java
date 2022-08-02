342874c242c55f5dc76523498874e03b988e3f3e
jucatupinamba <juniorbmx13@gmail.com>
2022-08-02 07:13:33 -0300

    feat: add Class Position - package boardgame

---
+++ src/boardgame/Position.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Position {
+
+    private int row;
+    private int column;
+
+    public Position(final int row, final int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public void setRow(final int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return this.column;
+    }
+
+    public void setColumn(final int column) {
+        this.column = column;
+    }
+
+    @Override
+    public String toString(){
+        return row + ", " + column;
+    }
+}
